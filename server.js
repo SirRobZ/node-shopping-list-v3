6a334f57224e42722d937e53f7a6a9033aa502fe
SirRobZ <sazarapkar@gmail.com>
2017-02-15 16:41:45 -0500

    added endpoint for delete recipe

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
 // so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+//ShoppingList.create('beans', 2);
+//ShoppingList.create('tomatoes', 3);
+//ShoppingList.create('peppers', 4);
 
 // adding some recipes to `Recipes` so there's something
 // to retrieve.
@@ -27,6 +27,7 @@ Recipes.create(
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
+/*
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
@@ -52,7 +53,11 @@ app.delete('/shopping-list/:id', (req, res) => {
   console.log(`Deleted shopping list item \`${req.params.id}\``);
   res.status(204).end();
 });
+*/
 
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
 
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
@@ -72,10 +77,14 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log('Deleted recipe list item \`${req.params.id}\`');
+  res.status(204).end();
+});
 
-app.get('/recipes', (req, res) => {
-  res.json(Recipes.get());
-})
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
